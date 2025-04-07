19134988c45b76345ccb0d4a29199dd11b06d174
Kuljot Biring <ksbiring1@gmail.com>
2025-04-06 17:37:30 -0700

    added code for variables subnet

--- main.tf
+++ main.tf
@@ -130,4 +130,14 @@ resource "aws_instance" "web" {
 }
 
 
+resource "aws_subnet" "variables-subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.250.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 
+  tags = {
+    Name      = "sub-variables-us-east-1a"
+    Terraform = "true"
+  }
+}
