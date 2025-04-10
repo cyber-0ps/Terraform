7e77255b9e5a196e2e0ba9ed1441435d76b1d422
Kuljot Biring <ksbiring1@gmail.com>
2025-04-09 20:48:36 -0700

    add local provider

--- terraform.tf
+++ terraform.tf
@@ -7,11 +7,15 @@ terraform {
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.1.0"
+      version = "3.7.1"
     }
     http = {
-      source = "hashicorp/http"
-      version = "2.1.0"
+      source  = "hashicorp/http"
+      version = "3.4.5"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.2"
     }
   }
 }
