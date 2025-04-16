2b95b22e050adf474c2cc25899c93e2ad3e033c9
Kuljot Biring <ksbiring1@gmail.com>
2025-04-15 20:46:50 -0700

    remove incorrect provisioners

--- main.tf
+++ main.tf
@@ -292,26 +292,13 @@ resource "aws_instance" "ubuntu_server" {
 
 }
 
-# Leave the first part of the block unchanged and create our `local-exec` provisioner
-  provisioner "local-exec" {
-    command = "chmod 600 ${local_file.private_key_pem.filename}"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo rm -rf /tmp",
-      "sudo git clone https://github.com/hashicorp/demo-terraform-101 /tmp",
-      "sudo sh /tmp/assets/setup-web.sh",
-    ]
-  }
-
-  # Terraform Resource Block - To Build Web Server in Public Subnet
+# Terraform Resource Block - To Build Web Server in Public Subnet
 resource "aws_instance" "web_server" {
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.public_subnets["public_subnet_1"].id
-  security_groups             = [aws_security_group.vpc-ping.id, 
-     aws_security_group.ingress-ssh.id, aws_security_group.vpc-web.id]
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  security_groups = [aws_security_group.vpc-ping.id,
+  aws_security_group.ingress-ssh.id, aws_security_group.vpc-web.id]
   associate_public_ip_address = true
   key_name                    = aws_key_pair.generated.key_name
   connection {
