09f394c1cba425da1ed061e7536d9e6a23fdbb4a
Kuljot Biring <ksbiring1@gmail.com>
2025-04-19 13:46:36 -0700

    add tags to provider block

--- main.tf
+++ main.tf
@@ -3,6 +3,13 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Owner       = "Acme"
+      Provisioned = "Terraform"
+    }
+  }
 }
 
 #Retrieve the list of AZs in the current AWS region
