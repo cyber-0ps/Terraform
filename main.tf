e7f1bf2f424944354cc50d263e6cfc879332fa13
Kuljot Biring <ksbiring1@gmail.com>
2025-04-21 21:30:12 -0700

    add second server module

--- main.tf
+++ main.tf
@@ -351,3 +351,14 @@ output "public_ip" {
 output "public_dns" {
   value = module.server.public_dns
 }
+
+module "server_subnet_1" {
+  source          = "./server"
+  ami             = data.aws_ami.ubuntu.id
+  subnet_id       = aws_subnet.public_subnets["public_subnet_1"].id
+  security_groups = [
+    aws_security_group.vpc-ping.id,
+    aws_security_group.ingress-ssh.id,
+    aws_security_group.vpc-web.id
+  ]
+}
