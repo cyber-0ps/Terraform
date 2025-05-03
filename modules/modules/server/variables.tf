e6d49baf4d03c140ecf01c6e622bf9be819a1da7
Kuljot Biring <ksbiring1@gmail.com>
2025-05-02 20:36:48 -0700

    corrected variables in file

--- modules/modules/server/variables.tf
+++ modules/modules/server/variables.tf
@@ -3,26 +3,6 @@ variable "size" {
   default = "t3.micro"
 }
 variable "subnet_id" {}
-
-variable "user" {}
-
-variable "security_groups" {
-  type = list(any)
-}
-
-variable "key_name" {
-
-}
-
-variable "private_key" {
-
-}
-
-variable "ami" {}
-variable "size" {
-  default = "t3.micro"
-}
-variable "subnet_id" {}
 variable "security_groups" {
   type = list(any)
 }
