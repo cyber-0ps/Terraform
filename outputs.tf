98262093903a6a8aa55523e802e5d2c51c92f7bc
Kuljot Biring <ksbiring1@gmail.com>
2025-05-11 18:26:29 -0700

    add ec2 instance arn to outputs

--- outputs.tf
+++ outputs.tf
@@ -22,3 +22,8 @@ output "public_ip" {
   description = "This is the public IP of my web server"
   value = aws_instance.web_server.public_ip
 }
+
+output "ec2_instance_arn" {
+  value = aws_instance.web_server.arn
+  sensitive = true
+}
