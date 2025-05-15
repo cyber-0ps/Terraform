48445e268881d63733ac1dfee161ba401d0f12b1
Kuljot Biring <ksbiring1@gmail.com>
2025-05-14 21:07:00 -0700

    choose first item in azs list

--- main.tf
+++ main.tf
@@ -485,6 +485,12 @@ locals {
   my_number = nonsensitive(var.phone_number)
 }
 
+resource "aws_subnet" "list_subnet" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.200.0/24"
+  availability_zone = var.us-east-1-azs[0]
+}
+
 output "cloud" {
   value = local.contact_info.cloud
 }
