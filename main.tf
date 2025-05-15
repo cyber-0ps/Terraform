9622a2cf19f0099fc1de88055fc2c1f3a7954a3b
Kuljot Biring <ksbiring1@gmail.com>
2025-05-14 21:02:55 -0700

    create new list and reference its values

--- main.tf
+++ main.tf
@@ -510,3 +510,14 @@ output "phone_number" {
   value     = var.phone_number
   sensitive = true
 }
+
+variable "us-east-1-azs" {
+    type = list(string)
+    default = [
+        "us-east-1a",
+        "us-east-1b",
+        "us-east-1c",
+        "us-east-1d",
+        "us-east-1e"
+    ]
+}
