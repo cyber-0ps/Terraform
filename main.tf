8ea7efff8058e4e2e98cd2ae8dd7fac3226317fb
Kuljot Biring <ksbiring1@gmail.com>
2025-05-24 15:45:20 -0400

    add outputs to see additional info

--- main.tf
+++ main.tf
@@ -557,3 +557,16 @@ resource "aws_iam_policy" "policy" {
     ]
   })
 }
+
+output "data-bucket-arn" {
+  value = data.aws_s3_bucket.data_bucket.arn
+}
+
+output "data-bucket-domain-name" {
+  value = data.aws_s3_bucket.data_bucket.bucket_domain_name
+}
+
+output "data-bucket-region" {
+  value = "The ${data.aws_s3_bucket.data_bucket.id} bucket is located 
+        in ${data.aws_s3_bucket.data_bucket.region}"
+}
