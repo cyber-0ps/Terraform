e9f93f3ce830ca17d43010af7e49600ae4780b04
Kuljot Biring <ksbiring1@gmail.com>
2025-05-24 16:38:02 -0400

    view resource blocks creating subnets

--- main.tf
+++ main.tf
@@ -609,3 +609,16 @@ locals {
     CreatedBy = lower(local.createdby)
   }
 }
+
+#Deploy the private subnets
+resource "aws_subnet" "private_subnets" {
+  for_each          = var.private_subnets
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
+  availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
+
+  tags = {
+    Name      = each.key
+    Terraform = "true"
+  }
+}
