eda13cd7852b998bfa1a2736659c8b2ba7126b69
Kuljot Biring <ksbiring1@gmail.com>
2025-05-25 12:00:18 -0400

    create resource graphs and dependencies

--- main.tf
+++ main.tf
@@ -689,3 +689,38 @@ resource "aws_security_group" "main" {
     }
   }
 }
+
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name        = var.vpc_name
+    Environment = "demo_environment"
+    Terraform   = "true"
+  }
+
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "private_subnets" {
+  for_each          = var.private_subnets
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
+  availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
+
+  tags = {
+    Name      = each.key
+    Terraform = "true"
+  }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "demo_igw"
+  }
+}
+
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+}
