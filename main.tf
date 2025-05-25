e13f00ca48e1982f8377adf72a7c8ba84016685d
Kuljot Biring <ksbiring1@gmail.com>
2025-05-25 11:25:25 -0400

    add security group resource

--- main.tf
+++ main.tf
@@ -622,3 +622,24 @@ resource "aws_subnet" "private_subnets" {
     Terraform = "true"
   }
 }
+
+resource "aws_security_group" "main" {
+  name   = "core-sg"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    description = "Port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
