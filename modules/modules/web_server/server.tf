10d93193e35f783bf20b52a2960949e796680987
Kuljot Biring <ksbiring1@gmail.com>
2025-05-26 13:20:03 -0400

    add module for s3 bucket

--- modules/modules/web_server/server.tf
+++ modules/modules/web_server/server.tf
@@ -1,22 +1,3 @@
-variable "ami" {}
-variable "size" {
-  default = "t3.micro"
-}
-variable "subnet_id" {}
-
-variable "user" {}
-
-variable "security_groups" {
-  type = list(any)
-}
-
-variable "key_name" {
-
-}
-
-variable "private_key" {
-
-}
 
 resource "aws_instance" "web" {
   ami                         = var.ami
